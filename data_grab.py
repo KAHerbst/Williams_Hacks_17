eb63710d2fa8259ea2ad958a5d6346bf57f2df34
KAHerbst <kah3@williams.edu>
2017-01-22 14:14:25 -0500

    Update data_grab.py

--- data_grab.py
+++ data_grab.py
@@ -6,7 +6,7 @@
 
 from xlrd import open_workbook
 import csv
-wb = open_workbook('el_Names.xls')
+wb = open_workbook(name)
 for i in range(wb.nsheets):
     sheet = wb.sheet_by_index(i)
     with open("data/%s.csv" %(sheet.name.replace(" ","")), "w") as file:
@@ -17,3 +17,7 @@ for i in range(wb.nsheets):
             row = [int(cell.value) if isinstance(cell.value, float) else cell.value
                    for cell in sheet.row(row_idx)]
             writer.writerow(row)
+            
+
+if __name__ == '__main__':
+    name = sys.argv[1]
